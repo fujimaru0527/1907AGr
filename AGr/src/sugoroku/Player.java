1c5d0e918589ded9215aa06b777df0775e945fe5
takiharamasaki <53197083+takiharamasaki@users.noreply.github.com>
2019-07-26 13:58:25 +0900

    プレイヤークラス変更
    プレイヤークラス変更

--- AGr/src/sugoroku/Player.java
+++ AGr/src/sugoroku/Player.java
@@ -4,12 +4,15 @@ public class Player {
 	public int point;
 	String name;
 	int num;
+	int position;
 
-	//初期値がポイント０、名前Player+nam
+	//初期値がポイント０、名前Player+nam、現在位置は北海道
 	public Player(int nam){
-		this.point=0;
+		this.point = 0;
 		this.num = nam;
-		this.name="Player"+this.num;
+		this.name ="Player"+this.num;
+		this.position = 0;
+		System.out.print("現在地は　" + position + "　北海道（仮）");
 
 	}
 	//ポイント加算
@@ -23,6 +26,11 @@ public class Player {
 	//ポイントゲッター
 	public int getPoint(){
 		return this.point;
+	}
+	//現在位置の表示
+	public int potision(){
+		System.out.print("現在位置は"+this.position+"地区");
+		return position;
 	}
 
 }
