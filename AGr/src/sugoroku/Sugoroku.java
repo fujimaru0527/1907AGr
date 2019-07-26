ee78cd964a2bae49db88d3137221af7ac6105aa1
Itokazuma-alterbo <itokazuma.alterbo@gmail.com>
2019-07-26 17:02:11 +0900

    Update Sugoroku.java

--- AGr/src/sugoroku/Sugoroku.java
+++ AGr/src/sugoroku/Sugoroku.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class Sugoroku {
 
-	//すごろくの長さ
+	// すごろくの長さ
 	ArrayList<String> sugoroku = new ArrayList<String>();
 
-	//sugorokuフィールドの作成
+	// sugorokuフィールドの作成
 	public Sugoroku() {
 
-		//すごろくにイベント発生↓
+		// すごろくにイベント発生↓
 		sugoroku.add(0, "スタート");
 		for (int i = 1; i <= 32; i++) {
 			/*
@@ -33,6 +33,44 @@ public class Sugoroku {
 			System.out.println(sugoroku.get(n));
 			return -1;
 		}
+	}
+
+	public void bord(int aPlayer, int bPlayer) {
+		System.out.println("すごろくマス目");
+		System.out.println("スタート｜北海道　｜　東北　｜　関東　｜　中部　｜　近畿　｜　中四国｜　九州　｜　沖縄　｜ゴール");
+		System.out.print("　　　　　");
+		for (int i = 1; i < 32; i++) {
+			System.out.printf("%2d", i);
+			if (i % 4 == 0) {
+				System.out.print("　");
+			}
+		}
+		System.out.println(" ");
+		if (aPlayer == 0) {
+			System.out.print("　　　");
+		} else {
+			System.out.print("　　　　　");
+		}
+		for (int i = 0; i < aPlayer; i++) {
+			System.out.print("　");
+			if (i % 4 == 0) {
+				System.out.print("　");
+			}
+		}
+		System.out.println("○");
+
+		if (aPlayer == 0) {
+			System.out.print("　　　");
+		} else {
+			System.out.print("　　　　　");
+		}
+		for (int i = 0; i < aPlayer; i++) {
+			System.out.print("　");
+			if (i % 4 == 0) {
+				System.out.print("　");
+			}
+		}
+		System.out.println("●");
 	}
 
 }
