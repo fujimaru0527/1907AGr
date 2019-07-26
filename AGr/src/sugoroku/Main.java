083676f875872d66eb209e77387f3795351981c9
Itokazuma-alterbo <itokazuma.alterbo@gmail.com>
2019-07-26 13:15:14 +0900

    Update Main.java

--- AGr/src/sugoroku/Main.java
+++ AGr/src/sugoroku/Main.java
@@ -3,8 +3,19 @@ package sugoroku;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// TODO 自動生成さたメソッド・スタブ
+		//プレーヤー情報生成
+		Player aPlayer = new Player(1);
+		Player bPlayer = new Player(2);
+
+
+
+
+
+
 
 	}
+
+
 
 }
