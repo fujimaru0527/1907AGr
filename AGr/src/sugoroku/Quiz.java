b84e4588875f2ac483b58b2a1bb55a1ffe88bafc
hirabayashiKano <52780060+hirabayashiKano@users.noreply.github.com>
2019-07-26 17:17:36 +0900

    クイズ更新
    クイズ更新

--- AGr/src/sugoroku/Quiz.java
+++ AGr/src/sugoroku/Quiz.java
@@ -6,7 +6,7 @@ public interface Quiz{
 }
 
 // 北海道クラス
-class Hokkaido implements Quiz {
+class Hokkaido1 implements Quiz {
 
 	@Override
 	public void quiz() {
@@ -37,8 +37,40 @@ class Hokkaido implements Quiz {
 
 }
 
-// 東北クラス
-class Touhoku implements Quiz {
+//北海道クラス２
+class Hokkaido2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("北海道にまつわるクイズです。");
+		System.out.println("北海道のJRの路線距離はいくつでしょう？か？");
+
+		System.out.println();
+
+		System.out.println("1:7400km");
+		System.out.println("2:2600km"); // 正解
+		System.out.println("3:857km");
+		System.out.println("4:2235km");
+	}
+
+	@Override
+	public int answer(int answer) {
+		switch (answer) {
+		case 2:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+
+}
+
+// 東北クラス1
+class Touhoku1 implements Quiz {
 
 	@Override
 	public void quiz() {
@@ -68,8 +100,39 @@ class Touhoku implements Quiz {
 	}
 }
 
-// 関東クラス
-class Kantou implements Quiz {
+//東北クラス2
+class Touhoku2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("東北にまつわるクイズです。");
+		System.out.println("東北地域において開発されたお米の品種は次のうちどれでしょうか？");
+
+		System.out.println();
+
+		System.out.println("1:ひとめぼれ"); //正解
+		System.out.println("2:ひめライス");
+		System.out.println("3:キヌヒカリ");
+		System.out.println("4:あさひの夢");
+	}
+
+	@Override
+	public int answer(int answer) {
+		switch (answer) {
+		case 1:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+}
+
+// 関東クラス1
+class Kantou1 implements Quiz {
 
 	@Override
 	public void quiz() {
@@ -100,8 +163,40 @@ class Kantou implements Quiz {
 	}
 }
 
-// 中部クラス
-class Tyubu implements Quiz {
+//関東クラス2
+class Kantou2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("群馬県にまつわるクイズです。");
+		System.out.println("「新川」　群馬県にあるこの地名はなんと読むでしょうか？");
+
+		System.out.println();
+
+		System.out.println("1:にっかわ");
+		System.out.println("2:しんかわ");
+		System.out.println("3:あらかわ"); //正解
+		System.out.println("4:にいがわ");
+	}
+
+	@Override
+	public int answer(int answer) {
+
+		switch (answer) {
+		case 3:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+}
+
+// 中部クラス1
+class Tyubu1 implements Quiz {
 
 	@Override
 	public void quiz() {
@@ -132,8 +227,40 @@ class Tyubu implements Quiz {
 	}
 }
 
-// 近畿クラス
-class Kinki implements Quiz {
+//中部クラス2
+class Tyubu2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("愛知県にまつわるクイズです。");
+		System.out.println("愛知県の県の花はなんでしょうか？");
+
+		System.out.println();
+
+		System.out.println("1:レンゲソウ");
+		System.out.println("2:ツツジ");
+		System.out.println("3:スイセン");
+		System.out.println("4:カキツバタ"); // 正解
+	}
+
+	@Override
+	public int answer(int answer) {
+
+		switch (answer) {
+		case 4:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+}
+
+// 近畿クラス1
+class Kinki1 implements Quiz {
 
 	@Override
 	public void quiz() {
@@ -164,8 +291,40 @@ class Kinki implements Quiz {
 	}
 }
 
-// 四国クラス
-class Shikoku implements Quiz {
+//近畿クラス2
+class Kinki2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("和歌山県にまつわるクイズです。");
+		System.out.println("和歌山の白浜にあるアドベンチャーで現在飼育されているパンダは何頭でしょう？");
+
+		System.out.println();
+
+		System.out.println("1:5匹");
+		System.out.println("2:3匹");
+		System.out.println("3:7匹");
+		System.out.println("4:2匹"); // 正解
+	}
+
+	@Override
+	public int answer(int answer) {
+
+		switch (answer) {
+		case 4:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+}
+
+// 四国クラス1
+class Shikoku1 implements Quiz {
 
 	@Override
 	public void quiz() {
@@ -197,8 +356,45 @@ class Shikoku implements Quiz {
 	}
 }
 
+//四国クラス2
+class Shikoku2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("愛媛県にまつわるクイズです。");
+		System.out.println("愛媛県の方言で、「机を移動させること」をなんというでしょう？");
+
+		System.out.println();
+
+		System.out.println("1:机をからう");
+		System.out.println("2:机をかく"); // 正解
+		System.out.println("3:机をなおす");
+		System.out.println("4:机をきる");
+	}
+
+	@Override
+	public int answer(int answer) {
+		// メインから入力してもらった値が来る
+
+		switch (answer) {
+		case 2:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+}
+
 // 九州クラス
-class kyusyu implements Quiz {
+<<<<<<< HEAD
+class kyusyu1 implements Quiz {
+=======
+class Kyusyu implements Quiz {
+>>>>>>> e4437a1731ceb89e23eef46b259ff6b9170e9d58
 
 	@Override
 	public void quiz() {
@@ -230,8 +426,40 @@ class kyusyu implements Quiz {
 	}
 }
 
-// 沖縄クラス
-class Okinawa implements Quiz {
+//九州クラス2
+class kyusyu2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("大分県にまつわるクイズです。");
+		System.out.println("温泉で有名な大分県別府市ですが、源泉数は世界何位でしょうか？");
+
+		System.out.println();
+
+		System.out.println("1:3位");
+		System.out.println("2:1位"); // 正解
+		System.out.println("3:5位");
+		System.out.println("4:10位");
+	}
+
+	@Override
+	public int answer(int answer) {
+
+		switch (answer) {
+		case 2:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+}
+
+// 沖縄クラス1
+class Okinawa1 implements Quiz {
 
 	@Override
 	public void quiz() {
@@ -253,6 +481,39 @@ class Okinawa implements Quiz {
 
 		switch (answer) {
 		case 4:
+			System.out.println("正解！+５ポイントGet!");
+			return 5;
+		default:
+			System.out.println("不正解！残念！");
+			return 0;
+		}
+
+	}
+}
+
+//沖縄クラス２
+class Okinawa2 implements Quiz {
+
+	@Override
+	public void quiz() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("沖縄県にまつわるクイズです。");
+		System.out.println("現在、沖縄にはいくつ世界遺産があるでしょうか？");
+
+		System.out.println();
+
+		System.out.println("1:9"); // 正解
+		System.out.println("2:7");
+		System.out.println("3:10");
+		System.out.println("4:5");
+	}
+
+	@Override
+	public int answer(int answer) {
+		// メインから入力してもらった値が来る
+
+		switch (answer) {
+		case 1:
 			System.out.println("正解！+５ポイントGet!");
 			return 5;
 		default:
