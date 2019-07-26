9a352ba10dee84d858e9d71d854aa4edae21fb25
Itokazuma-alterbo <itokazuma.alterbo@gmail.com>
2019-07-26 14:56:16 +0900

    Update Main.java

--- AGr/src/sugoroku/Main.java
+++ AGr/src/sugoroku/Main.java
@@ -16,34 +16,35 @@ public class Main {
 		listPlayer.add(aPlayer);
 		listPlayer.add(bPlayer);
 
+		boolean nextGame
+
 		System.out.print("ゲーム開始");
 
 
 		Sugoroku sugoroku = new Sugoroku();
+		int playerNum = 1;
+		while(1){
 
-		System.out.print(aPlayer.name+"の順番");
+		System.out.print(listPlayer.get(playerNum).name+"の順番");
 
 		Dice dice = new Dice();
 		//プレーヤーを進める
-		aPlayer.position += dice.getNum();
 
-		int point sugoroku.(aPlayer.position);
+		//初期値
+		int start = listPlayer.get(playerNum).position;
+		listPlayer.get(playerNum).position += dice.getNum();
 
-
-
-
-
+		listPlayer.get(playerNum).setPoint(sugoroku.judge(listPlayer.get(playerNum).position));
 
 
-
-
-
-
-
-
-
+			if(listPlayer.get(playerNum).position > 32){
+				break;
+			}
+		}
 
 	}
+
+	if()
 
 
 
